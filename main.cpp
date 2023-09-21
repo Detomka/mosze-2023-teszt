dc34e287623647a7bb39c8d773417609f3dac994
Detomka <145669263+Detomka@users.noreply.github.com>
2023-09-21 13:36:24 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[N_ELEMENTS];             //NELEMENTS forma nem megfelelő helyette N_ELEMENTS
+    std::cout << "1-100 ertekek duplazasa";    //idézőjelben kell lennie a szövegnek
+    for (int i = 0; i < N_ELEMENTS; i++)                          // megkell adni, hogy meddig megyünk i-vel és milyen lépésközzel
     {
-        b[i] = i * 2;
+        b[i] = b[i] * 2;                          // nem i*2-vel egyenlő a b[i] hanem b[i]*2 vel
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i < N_ELEMENTS; i++)                      // nincs megadva meddig megyünk i-vel
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" << b[i] << endl;                           // az érték maga nem volt kiíratva
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    int atlag;                                               
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i];                                         // ";" lemaradt a végéről
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
