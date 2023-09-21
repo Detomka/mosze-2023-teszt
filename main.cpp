cff47c776ff5673effc86f4a6dfaa3ca052a1318
Detomka <145669263+Detomka@users.noreply.github.com>
2023-09-21 13:52:40 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,11 +12,11 @@ int main()
     }
     for (int i = 0; i < N_ELEMENTS; i++)                      // nincs megadva meddig megyünk i-vel
     {
-        std::cout << "Ertek:" << b[i] << endl;                           // az érték maga nem volt kiíratva
+        std::cout << "Ertek:" << b[i] <<std:: endl;                           // az érték maga nem volt kiíratva
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;                                               
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         atlag += b[i];                                         // ";" lemaradt a végéről
     }
